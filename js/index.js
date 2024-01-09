5cd7aafb9537ee6bd472c59bfb5dee676dd0a871
svertkatter <alice241102@gmail.com>
2024-01-09 18:16:52 +0900

    編集

--- js/index.js
+++ js/index.js
@@ -1,26 +1,5 @@
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
-let circleX = canvas.width / 2;
-let circleY = canvas.height / 2;
-let circleRadius = 20;
 let ws = new WebSocket('wss://our-socket-server.onrender.com:443');
 let selectValue = null;
-
-function drawCircle() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.beginPath();
-    ctx.arc(circleX, circleY, circleRadius, 0, Math.PI * 2);
-    ctx.fillStyle = 'blue';
-    ctx.fill();
-    ctx.closePath();
-}
-
-function updateCircleSize(acceleration) {
-    const accelerationMagnitude = Math.sqrt(acceleration.x ** 2 + acceleration.y ** 2 + acceleration.z ** 2);
-    if (accelerationMagnitude > 5) {
-        circleRadius += 5;
-    }
-}
 
 function sendAccelerationData(acceleration) {
     const dataToSend = {
@@ -39,9 +18,7 @@ function setupDeviceMotion() {
             y: event.acceleration.y,
             z: event.acceleration.z
         };
-        updateCircleSize(acceleration);
         sendAccelerationData(acceleration);
-        drawCircle();
     });
 }
 
@@ -68,9 +45,5 @@ document.getElementById('permissionButton').addEventListener('click', function (
 });
 
 document.getElementById('resetButton').addEventListener('click', function () {
-    circleRadius = 20;
-    drawCircle();
     selectValue = 0;
 });
-
-drawCircle();
