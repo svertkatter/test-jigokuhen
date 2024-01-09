6daeadd5236efdbae7732996744424eb2ce1ab0b
svertkatter <alice241102@gmail.com>
2024-01-09 20:03:19 +0900

    test

--- js/anime.js
+++ js/anime.js
@@ -1,29 +1,20 @@
-// anime.js
-function animateBackground() {
-    // 背景用のdiv要素を作成
-    const bgDiv = document.createElement('div');
-    bgDiv.style.position = 'fixed';
-    bgDiv.style.top = '0';
-    bgDiv.style.left = '0';
-    bgDiv.style.width = '100%';
-    bgDiv.style.height = '100dvh';
-    bgDiv.style.backgroundColor = 'white';
-    bgDiv.style.opacity = '0';
-    bgDiv.style.zIndex = '9999';
-    document.body.appendChild(bgDiv);
+const isOpen = document.querySelector('.open');
 
-    // GSAPを使用して背景の透明度をアニメーション
-    gsap.to(bgDiv, {
-        duration: 1, opacity: 1, onComplete: () => {
-            // アニメーション完了後、背景を非表示に
-            gsap.to(bgDiv, {
-                duration: 1, opacity: 0, delay: 1, onComplete: () => {
-                    bgDiv.parentNode.removeChild(bgDiv);
-                }
-            });
-        }
-    });
-}
+const tl = gsap.timeline({ paused: true });
 
-// この関数を外部から呼び出せるようにエクスポート
-export { animateBackground };
+tl.fromTo(
+    //メニューを上から下へ表示
+    ".back",
+    {
+        autoAlpha: 0,
+    },
+    {
+        autoAlpha: 1,
+        duration: 1,
+        ease: Power2.easeInOut,
+    }
+);
+
+isOpen.addEventListener("click", () => {
+    tl.play()
+});
